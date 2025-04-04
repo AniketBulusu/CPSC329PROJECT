17da20aad1cf2bb1d9918d711a45299fccc005bb
San <sanlee1021@gmail.com>
2025-04-04 02:54:12 -0600

    aghhhhhhhhhhhhhhhh

---
+++ CPSC329GP/renderer/pages/dashboard.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { PasswordEntry } from "../types/electron";
+import { useRouter } from "next/router";
+
+export default function DashboardPage(){
+    const [entries, setEntries] = useState<PasswordEntry[]>([])
+    const [newEntry, setNewEntry] = useState<PasswordEntry>({
+        id: '',
+        service: '',
+        username: '',
+        password: '',
+    })
+    const [error, setError] = useState('')
+    const router = useRouter()
+
+    const currentUser = typeof window !== 'undefined' ? localStorage.getItem('currentUser') : null
+
+    useEffect(() => {
+        const currentUser = localStorage.getItem('currentUser')
+        if(!currentUser){
+            router.push('/login')
+            return
+        }
+
+        async function fetchEntries(){
+            try{
+                const fetchedEntries = await window.electron.getEntries(currentUser)
+                setEntries(fetchedEntries)
+            }catch (err){
+                setError('Failed to load entries')
+            }
+        }
+        fetchEntries()
+    }, [currentUser, router])
+
+    const handleAddEntry = async(e: React.FormEvent) => {
+        e.preventDefault()
+        if(!currentUser) {return}
+
+        const id = Date.now.toString()
+        const entry: PasswordEntry = { ...newEntry, id}
+
+        try{
+            const succes = await window.electron
+        }catch(error){
+
+        }
+    }
+}
